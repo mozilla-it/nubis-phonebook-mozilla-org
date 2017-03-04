a649e780d94aed33bbb35769e22fa5a8dda6139b
Daniel Hartnell <danielhartnell@gmail.com>
2017-03-03 16:16:37 -0800

    provides a simple website status page

--- nubis/puppet/copy-site.pp
+++ nubis/puppet/copy-site.pp
@@ -1,9 +1,9 @@
-# Copy index page to Apache web root
+# Copy status page to Apache web root
 
-file {'/var/www/html/index.htm':
-	ensure => file,
-	owner	 => root,
-	group  => root,
-	mode   => '0755',
-	source => 'puppet:///nubis/files/index.htm',
+file {'/var/www/html/200.html':
+  ensure => file,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+  source => 'puppet:///nubis/files/200.html',
 }
