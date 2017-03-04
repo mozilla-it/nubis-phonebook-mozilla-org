efca2b4cc9ba24ba375f5274e4cbb070b4b3b5a7
Daniel Hartnell <danielhartnell@gmail.com>
2017-03-03 16:39:19 -0800

    removed unnecessary html file

--- nubis/puppet/copy-site.pp
+++
@@ -1,9 +0,0 @@
-# Copy status page to Apache web root
-
-file {'/var/www/html/200.html':
-  ensure => file,
-  owner  => root,
-  group  => root,
-  mode   => '0755',
-  source => 'puppet:///nubis/files/200.html',
-}
