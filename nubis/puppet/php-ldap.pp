06cfc5adb4758af3eeea207022d9b0c989beb99c
Daniel Hartnell <danielhartnell@gmail.com>
2017-03-22 16:19:54 -0700

    evaluating ldap dependencies

--- nubis/puppet/php-ldap.pp
+++ nubis/puppet/php-ldap.pp
@@ -4,3 +4,13 @@ package { 'php5-ldap':
     ensure => present,
     name   =>  'php5-ldap'
 }
+
+package { 'libldap':
+    ensure => present,
+    name   => 'libldap'
+}
+
+package { 'openldap':
+    ensure => present,
+    name   => 'openldap'
+}
